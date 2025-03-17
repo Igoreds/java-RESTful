9cadff977cf622283abff25d9ae3e094fcf88c2c
Igoreds <igoreds1999@gmail.com>
2025-03-17 01:56:22 -0300

    Project

---
+++ src/main/java/me/dio/Project_Restful_java/ProjectRestfulJavaApplication.java
@@ -0,0 +1,13 @@
+package me.dio.Project_Restful_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRestfulJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRestfulJavaApplication.class, args);
+	}
+
+}
