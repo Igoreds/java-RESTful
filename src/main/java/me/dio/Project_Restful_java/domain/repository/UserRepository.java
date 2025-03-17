c9e18d33989221a3198afc4de2cbd8ae7538d023
Igoreds <igoreds1999@gmail.com>
2025-03-17 11:53:28 -0300

    Hotflix precision e scale

---
+++ src/main/java/me/dio/Project_Restful_java/domain/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package me.dio.Project_Restful_java.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.Project_Restful_java.domain.model.User;
+
+
+@Repository
+public interface UserRepository  extends JpaRepository<User, Long> {
+
+    
+    
+}
